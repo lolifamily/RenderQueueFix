8a8bc21a7837b5cde85a424b4ae09c09d1be8bc1
lolifamily <lolifamilies@gmail.com>
2026-06-06 17:01:45 +0800

    Fix missing using directive for AssemblyVersion attributes
    The assembly-level [AssemblyVersion] / [AssemblyFileVersion] attributes
    under the !DEV_BUILD branch reference types from System.Reflection but
    the using directive was absent, so any non-DEV_BUILD compile (i.e. the
    Pulsar path this block exists for) would fail with CS0246.
    
    Add `using System.Reflection;` inside the same #if !DEV_BUILD block so
    the directive only appears when the attributes that need it are also
    compiled in.

--- ClientPlugin/Plugin.cs
+++ ClientPlugin/Plugin.cs
@@ -2,6 +2,7 @@
 
 // Set the assembly version manually if compiled by Pulsar (it won't create what was in AssemblyInfo.cs before)
 #if !DEV_BUILD
+using System.Reflection;
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
 #endif
